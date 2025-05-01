02bc3378af00258c5b9669146b416cef9932e156
SAT-587 <shiva.subramoney@gmail.com>
2025-05-01 14:11:09 +0200

    Update house_price_app.py

--- house_price_app.py
+++ house_price_app.py
@@ -118,4 +118,5 @@ input_data = pd.DataFrame([{
 # Predict button
 if st.button("Predict"):
     prediction = model.predict(input_data)
-    st.write(f"The predicted price of the house is ${(prediction)[0]:.2f}.")
+    formatted_price = f"${prediction[0]:,.2f}"
+    st.write(f"The predicted price of the house is {formatted_price}.")
